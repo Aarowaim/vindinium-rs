b4133334bcef3b8c5b9a587880a2a3b760dc6371
Aaron Lip <kitsunay34@gmail.com>
2017-11-04 01:51:08 -0600

    Concurrent actors now work

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ pub mod user;
 extern crate rand;
 use rand::Rng;
 
-extern crate chan;
+extern crate multiqueue;
 extern crate futures;
 extern crate tokio_core;
 
@@ -36,23 +36,6 @@ enum Message {
 	Terminate,
 }
 
-struct RX<T>(chan::Receiver<T>);
-impl<T> futures::stream::Stream for RX<T> {
-	type Item = T;
-	type Error = Box<std::error::Error>;
-
-	fn poll(&mut self) -> futures::Poll<Option<Self::Item>, Self::Error> {
-		let &mut RX(ref receiver) = self;
-		let item = receiver.recv();
-		match item {
-			Some(value) => Ok(futures::Async::Ready(Some(value))),
-			None => Ok(futures::Async::NotReady),
-		}
-		
-	}
-
-}
-
 fn main() {
 	let key_file = "key.txt";
 	let mut f = File::open(key_file)
@@ -61,7 +44,7 @@ fn main() {
 	let mut key = String::new();
 	f.read_to_string(&mut key)
 		.expect(format!("Could not read '{}'.", key_file).as_str());
-	key.trim()
+	key.trim();
 
 	let mut client = Client::new("http://vindinium.org".to_string(), 
 		key, RandomBot{});
@@ -79,16 +62,37 @@ fn main() {
 use futures::{Stream, Future};
 
 fn my_test() {
-	let mut core = tokio_core::reactor::Core::new().expect("Could not instantiate reactor");
-	let handle = core.handle();
+	let (tx, rx) = multiqueue::broadcast_fut_queue(60000);
 
-	let (tx, rx) = chan::async::<String>();
-
-	tx.send("Hello".to_string());
-	let incoming = RX(rx).for_each(|s| {
-		println!("Result: {}", s);
-		Ok(())
-	});
-
-	core.run(incoming).expect("Could not run reactor event loop")
+	for name in vec!["A", "B", "C"] {
+		my_test_receiver(tx.clone(), rx.add_stream(), name.to_string());
+	}
+	
+	let delay = std::time::Duration::from_millis(100);
+	let mut count = 0usize;
+	loop {
+		tx.try_send(format!("{} #{}", "Message", count)).expect("Failed to send");
+		std::thread::sleep(delay);
+		count += 1;
+	}
 }
+
+//TODO: This will probably require a macro or callback to inject user code
+fn my_test_receiver(send: multiqueue::BroadcastFutSender<String>, recv: multiqueue::BroadcastFutReceiver<String>, name: String) {
+	std::thread::spawn(move || {
+		let mut core = tokio_core::reactor::Core::new().expect("Could not instantiate reactor");
+		let handle = core.handle();
+
+		let incoming = recv.for_each(|s| {
+			println!("On thread '{}': {}", name, s);
+			Ok(())
+		});
+
+		core.run(incoming).expect("Could not run reactor event loop")
+	});
+}
+//TODO: Use `Service` to generate futures containing "Hello" or something
+/*fn my_test_sender(send: multiqueue::MPMCFutSender<String>, recv: multiqueue::MPMCFutReceiver<String>) {
+	std::thread::spawn(|| {
+	});
+}*/
